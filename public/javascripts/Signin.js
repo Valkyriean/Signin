f26f26ad8bcdb95fc54be638a677074a4130a8fd
Valkyriean <phantomgale@hotmail.com>
2016-11-09 23:24:42 +0800

    All Code Done!!!

--- public/javascripts/Signin.js
+++ public/javascripts/Signin.js
@@ -12,27 +12,71 @@ function goodPassword(str){
 
 var init = function() {
 
-    $("#Button").click(function(e) {
+    $("#submit").click(function(e) {
         e.preventDefault();
         var email = document.getElementById("emailInput").value;
         var firstname = document.getElementById("firstnameInput").value;
         var lastname = document.getElementById("lastnameInput").value;
         var password = document.getElementById("passwordInput").value;
-        var verifypassword = document.getElementById("verifpasswordInput").value;
+        var verifypassword = document.getElementById("verifypasswordInput").value;
 
         if(!isEmail(email)){
             //wrong email
+            alert('Invalid Email Address');
         }else if(!goodPassword(password)){
             //not good password
+            alert('Invalid Password');
         }else if(password!=verifypassword){
             //not same password
+            alert('Two Password Are Not Same');
         }else{
             //all good
             var data = {email:email,firstname:firstname,lastname:lastname,password:password,verifypassword:verifypassword};
+
+
             $.post(url,data,function (response) {
                 console.log(response);
-                alert('login '+response.status);
+                var res = response.status;
+                if(res=='we'){
+                    alert('Invalid Email Address');
+                }else if(res=='wp'){
+                    alert('Invalid Password');
+                }else if(res == 'dp'){
+                    alert('Two Password Are Not Same');
+                }else if(res=='success'){
+                    alert('Sign In Success');
+                }else{
+                    alert('WTF???');
+                }
             },'json');
+
+            /* substituted solution
+            var settings = {
+                "url": url,
+                "method": "POST",
+                "headers": {
+                    "content-type": "application/json",
+                },
+                "data": data
+            }
+            $.ajax(settings).done(function (response) {
+                console.log(response.status);
+                var res = response.status;
+                if(res=='we'){
+                    alert('Invalid Email Address');
+                }else if(res='wp'){
+                    alert('Invalid Password');
+                }else if(res = 'dp'){
+                    alert('Two Password Are Not Same');
+                }else if(res='success'){
+                    alert('Sign In Success');
+                }else{
+                    alert('WTF???');
+                }
+            });
+            */
+
+
         }
 
 
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,16 @@
 var express = require('express');
 var router = express.Router();
 
+function isEmail(str){
+    var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
+    return reg.test(str);
+}
+
+function goodPassword(str){
+    var reg =/^[\@A-Za-z0-9\!\#\$\%\^\&\*\.\~]{6,22}$/;
+    return reg.test(str);
+}
+
 router.post('/login', function (req, res) {
     console.log("login connected");
     var username = req.body.username;
@@ -14,19 +24,39 @@ router.post('/login', function (req, res) {
         res.json({"status": "success"});
         console.log("success");
     } else {
-        res.send({"status":"failed"});
+        res.json({"status":"failed"});
         console.log("failed");
     }
 });
 
 router.post('/signin',function(req,res) {
     console.log("Sign connected");
+
     var email = req.body.email;
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
     var password = req.body.password;
     var verifypassword = req.body.verifypassword;
 
+    if(!isEmail(email)){
+        //wrong email
+        res.json({"status": "we"});
+        console.log("wrong email");
+    }else if(!goodPassword(password)){
+        //not good password
+        res.json({"status": "wp"});
+        console.log("wrong password");
+    }else if(password!=verifypassword){
+        //not same password
+        res.json({"status": "dp"});
+        console.log("different password");
+    }else{
+        //all good
+        res.json({"status": "success"});
+        console.log("success");
+
+        //add code to save in data base here
+    }
 });
 
 
