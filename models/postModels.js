0507914f20c0d9b0c6e2cb526258e6cb005c8509
Valkyriean <phantomgale@hotmail.com>
2016-11-16 07:58:06 +0800

    Models

--- models/postModels.js
+++ models/postModels.js
@@ -2,4 +2,17 @@
  * Created by Alexander on 14/11/2016.
  */
 
+// grab the things we need
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var signup = new Schema({
+    emailaddress: String,
+    firstname: String,
+    lastname: String,
+    password: { type: String, required: true }
+});
+
+
+module.exports = mongoose.model('Signup', signup);
