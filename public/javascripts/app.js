685d76b58238c8014015ad607e1846a0487d9774
JoshMYan <yanboxiao1999@126.com>
2016-11-30 07:52:18 +0800

    scope.go

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,19 +4,26 @@ app.config(function($stateProvider,$urlRouterProvider) {
     var login = {
         name: 'login',
         url: '/login',
-        controller: 'MainCtrl',
+        controller: 'Ctrl',
         templateUrl: 'template/Login.html'
     };
 
-    var signin = {
-        name: 'signin',
-        url: '/signin',
+    var signup = {
+        name: 'signup',
+        url: '/signup',
+        controller: 'Ctrl',
         templateUrl: 'template/SignUp.html'
     };
 
     $stateProvider.state(login);
-    $stateProvider.state(signin);
+    $stateProvider.state(signup);
 
     $urlRouterProvider.otherwise('/login');
 
+});
+
+app.controller('Ctrl',function ($scope,$location) {
+    $scope.go = function (add) {
+        $location.path(add);
+    }
 });
